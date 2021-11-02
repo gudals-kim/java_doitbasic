dbbd6d1c169106a9ff682619debd52cf4ee40717
kimgudals <gudals-kim@github.com>
2021-11-02 14:50:33 +0900

    [update] 생성자에서 다른 생성자를 호출하는 this

--- src/basic/classPart3/BirthDay.java
+++ src/basic/classPart3/BirthDay.java
@@ -5,10 +5,16 @@ public class BirthDay {
     int month;
     int year;
 
-    public void setYear(int year){
+    public BirthDay(){
+        this(1992, 10, 20);
+    }
+    public BirthDay(int year, int month, int day){
         this.year = year;
+        this.month = month;
+        this.day = day;
     }
-    public void printThis(){
-        System.out.println(this);
+    public void printBirthDay(){
+        System.out.println("생일은 "+this.year+"년"+this.month+"월"+this.day+"일 입니다.");
     }
+
 }
--- src/basic/classPart3/ThisTest.java
+++ src/basic/classPart3/ThisTest.java
@@ -3,8 +3,9 @@ package basic.classPart3;
 public class ThisTest {
     public static void main(String[] args) {
         BirthDay dDay = new BirthDay();
-        dDay.setYear(1998);
-        System.out.println(dDay);
-        dDay.printThis();
+        System.out.println(dDay.year);
+        System.out.println(dDay.month);
+        System.out.println(dDay.day);
+        dDay.printBirthDay();
     }
 }
