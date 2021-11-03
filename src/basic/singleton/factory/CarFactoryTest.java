a1b431d7fbc01a3b9713a1b174ed5e06a8e61203
kimgudals <gudals-kim@github.com>
2021-11-03 17:41:51 +0900

    [fix] sington patton Ex.2

--- src/basic/singleton/factory/CarFactoryTest.java
+++ src/basic/singleton/factory/CarFactoryTest.java
@@ -4,7 +4,7 @@ public class CarFactoryTest {
     public static void main(String[] args) {
         Car k5 = new Car();
         Car k3 = new Car();
-        System.out.println(k5.num);
-        System.out.println(k3.num);
+        System.out.println("K5의 차량 제작 번호 : "+k5.num);
+        System.out.println("k3의 차량 제작 번호 : "+k3.num);
     }
 }
