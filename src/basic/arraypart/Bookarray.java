bdd8973982b77f770bc3a1b882e8053c04b3c204
kimgudals <gudals-kim@github.com>
2021-11-04 16:38:28 +0900

    [update]객체 배열 연습

---
+++ src/basic/arraypart/Bookarray.java
@@ -0,0 +1,11 @@
+package basic.arraypart;
+
+public class Bookarray {
+    public static void main(String[] args) {
+        Book library[] = new Book[5];
+
+        for(int i=0; i< library.length; i++){
+            System.out.println(library[i]);
+        }
+    }
+}
